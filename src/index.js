afcd4a9d9772dd353d8c067159201a4449659fb4
Victor Maia <srvictormaia@gmail.com>
2017-10-17 20:57:53 -0200

    Clear storage when new version

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+// TODO (in a future): migrations
+const version = "0.1.0";
+const userVersion = window.localStorage.getItem("moonx-version");
+if (userVersion !== version) {
+  window.localStorage.clear();
+  window.localStorage.setItem("moonx-version", version);
+}
+
 const Inferno = require("inferno");
 const Browser = require("./components/browser.js");
 
