84482fb61c48e0d7c948f996909e943d275bab5d
Victor Maia <srvictormaia@gmail.com>
2017-10-18 19:39:18 -0200

    Use latest wallet

--- src/components/browser.js
+++ src/components/browser.js
@@ -25,7 +25,7 @@ module.exports = createClass({
     this.activeAppDataNonce = 0;
     this.localDataKey = "mist-lite-data";
     this.homeAppCid = "zb2rhmqj5QpYzhL3DXGBfJDMrX1cwmNHh6SQ7zbgachqenNqp";
-    this.walletAppCid = "zb2rhavCWDAW2vEbAAJpAvzRu4mPXxJUHRSPdvLUDgtuaKEzY";
+    this.walletAppCid = "zb2rhXmvuB3AjdagBX7556Pdmss7X4GD1u32dPbUAXTR1ka5Y";
 
     window.acc = pvt => {
       const acc = Eth.account.fromPrivate(pvt);
